4cc02bfe5835c94f97f1b3ffa9a0a75e886a8a7f
Shen Zhifeng <shen_zhifeng@outlook.com>
2019-11-19 09:41:01 +0800

    test

---
+++ VoteDemo/XML/TeamService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using VoteDemo.Models;
+
+namespace VoteDemo.XML
+{
+    public class TeamService
+    {
+        public async Task<IList<VoteInfo>> GetTeamVoteInfosAsync()
+        {
+            IList<VoteInfo> voteInfos=new List<VoteInfo>();
+            try
+            {
+                //XDocument document = XDocument.LoadAsync("asdas");
+                XmlDocument document =new  XmlDocument();
+                document.Load("./wwwroot");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            return voteInfos;
+        }
+
+
+    }
+}
